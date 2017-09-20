1c71dc079e2a91cc113b6a475cafcd3227e09c6e
Michael Kane <mediocrecomedy@gmail.com>
2017-09-19 17:09:09 -0700

    update docstrings for each function

--- Hangman_Project/ps3_hangman_template.py
+++ Hangman_Project/ps3_hangman_template.py
@@ -4,84 +4,76 @@ def loadWord():
    f = open('words.txt', 'r')
    wordsList = f.readlines()
    f.close()
-   
+
    wordsList = wordsList[0].split(' ')
    secretWord = random.choice(wordsList)
    return secretWord
-    
-    
-    
-  
-    
-    
-    
-    
-    
-        
-                
+
+
+
+
+
+
+
+
+
+
+
 
 def isWordGuessed(secretWord, lettersGuessed):
     '''
-    secretWord: string, the word the user is guessing
-    lettersGuessed: list, what letters have been guessed so far
-    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+    secretWord: string, the random word the user is trying to guess.  This is selected on line 9.
+    lettersGuessed: list of letters that have been guessed so far.
+    returns: boolean, True only if all the letters of secretWord are in lettersGuessed;
       False otherwise
     '''
     # FILL IN YOUR CODE HERE...
-    
+
 
 
 
 def getGuessedWord(secretWord, lettersGuessed):
     '''
-    secretWord: string, the word the user is guessing
-    lettersGuessed: list, what letters have been guessed so far
-    returns: string, comprised of letters and underscores that represents
-      what letters in secretWord have been guessed so far.
+    secretWord: string, the random word the user is trying to guess.  This is selected on line 9.
+    lettersGuessed: list of letters that have been guessed so far.
+    returns: string, of letters and underscores.  For letters in the word that the user has
+    guessed correctly, the string should contain the letter at the correct position.  For letters
+    in the word that the user has not yet guessed, shown an _ (underscore) instead.
     '''
     # FILL IN YOUR CODE HERE...
-    
+
 
 
 
 def getAvailableLetters(lettersGuessed):
     '''
-    lettersGuessed: list, what letters have been guessed so far
+    lettersGuessed: list of letters that have been guessed so far
     returns: string, comprised of letters that represents what letters have not
       yet been guessed.
     '''
-    
-    
+
+
 
 def hangman(secretWord):
     '''
     secretWord: string, the secret word to guess.
 
-    Starts up an interactive game of Hangman.
+    Starts up a game of Hangman in the command line.
 
-    * At the start of the game, let the user know how many 
+    * At the start of the game, let the user know how many
       letters the secretWord contains.
 
-    * Ask the user to supply one guess (i.e. letter) per round.
+    * Ask the user to guess one letter per round.
 
-    * The user should receive feedback immediately after each guess 
-      about whether their guess appears in the computers word.
+    * The user should receive feedback immediately after each guess
+      about whether their guess appears in the computer's word.
 
-    * After each round, you should also display to the user the 
-      partially guessed word so far, as well as letters that the 
+    * After each round, you should also display to the user the
+      partially guessed word so far, as well as letters that the
       user has not yet guessed.
-
-    Follows the other limitations detailed in the problem write-up.
     '''
     # FILL IN YOUR CODE HERE...
-    
 
 
-
-
-# When you've completed your hangman function, uncomment these two lines
-# and run this file to test! (hint: you might want to pick your own
-# secretWord while you're testing)
-
-#secretWord = loadWord()
-#hangman(loadWord())
+secretWord = loadWord()
+hangman(loadWord())

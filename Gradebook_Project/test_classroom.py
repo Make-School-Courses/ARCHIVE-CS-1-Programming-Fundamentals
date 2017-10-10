9ed184fa72450616510f426c960c54f198aac6ec
Michael Kane <mediocrecomedy@gmail.com>
2017-10-09 22:57:12 -0700

    add methods stubs

---
+++ Gradebook_Project/test_classroom.py
@@ -0,0 +1,33 @@
+from student import Student
+from classroom import Classroom
+import pytest
+
+def setup_for_test():
+    pass
+
+def test_setup():
+    pass
+
+def test_enroll_student():
+    pass
+
+def test_add_assignment_for_student():
+    pass
+
+def test_add_assignment_for_class():
+    pass
+
+def test drop_assignment_for_student():
+    pass
+
+def test_drop_assignment_for_class():
+    pass
+
+def test__is_valid_grade():
+    pass
+
+def test_get_student_GPA():
+    pass
+
+def test_get_class_average():
+    pass

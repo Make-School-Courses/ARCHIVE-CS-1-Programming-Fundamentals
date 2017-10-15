4db833f7b25711ec4783ba287041ced4f9cdd6bd
Michael Kane <mediocrecomedy@gmail.com>
2017-10-16 00:05:06 +0100

    complete framework for students to begin work on

--- Herd_Immunity_Project/virus.py
+++ Herd_Immunity_Project/virus.py
@@ -1,2 +1,28 @@
-class virus(object):
-    pass
+class Virus(object):
+    '''
+    _____Attributes______:
+
+    name: String.  The name of the virus.
+
+    mortality_rate: Float between 0 and 1.  Represents the percentage chance that
+        a person infected with this virus will die from this infection.
+
+    basic_repro_num:  Float between 0 and 1.  Represents the average number of people a person
+    will infect during the course of their illness.
+
+    NOTE: For simplicity's sake, this simulation will assume that basic_repro_num is
+    based on the number of infections spread out of contacts with 100 people from
+    the larger population over the course of the infection.  This is an over-simplification
+    of how basic reproduction number works, but it will do for our program.
+
+    _____Methods_____
+
+    __init__(self, name, mortality_rate, basic_repro_num)
+        - All attributes are passed as parameters and set at instantiation.
+    '''
+
+def __init__(self, name, mortality_rate, basic_repro_num):
+    # TODO: Finish this method.  Set all attributes to their corresponding parameters.
+    self.name = None
+    self.mortality_rate = None
+    self.basic_repro_num = None
